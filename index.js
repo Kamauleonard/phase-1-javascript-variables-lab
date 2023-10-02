d3ae16d307d373327df3b042d3e082418e0006f4
Kamauleonard <leonardmwangi2012@gmail.com>
2023-10-02 15:35:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-// Code your solution in this file!
+// Solution 1
+let companyName = "Scuber";
+
+function name() {
+    console.log(companyName);
+}
+
+name();
+
+// Solution 2
+const companyName2 = "Scuber";
+
+function name2() {
+    console.log(companyName2);
+}
+
+name2();
+
+// Solution 3
+const mostProfitableNeighborhood = "Chelsea";
+
+function defineMostProfitableNeighborhood() {
+    console.log(mostProfitableNeighborhood);
+}
+
+// Calling the function
+defineMostProfitableNeighborhood();
+
+
+
+
+
+
+// Solution 4
+function defineMostProfitableNeighborhood() {
+    let mostProfitableNeighborhood = "Chelsea";
+    console.log(`The most profitable neighborhood is: ${mostProfitableNeighborhood}`);
+}
+
+// Calling the function
+defineMostProfitableNeighborhood();
+
+
+
+
+
+// Solution 5
+const companyCeo = "Susan Smith";
+
+function definecompanyCeo() {
+    console.log(companyCeo);
+}
+
+// Calling the function
+definecompanyCeo();
+
+
+
+
+
+
+// Solution 6
+
+let companyCeo2 = "Susan Smith";
+
+function definecompanyCeo() {
+    console.log(companyCeo);
+}
+
+// Calling the function
+definecompanyCeo();
